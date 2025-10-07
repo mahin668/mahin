201814f43018ea95b42c13280f22ac2064d4a9c0
webocean <webocean@weboceans-Mac-mini-2.local>
2025-10-07 09:19:05 +0600

    added code

---
+++ script.js
@@ -0,0 +1,11 @@
+var numbers = [1,2,3,4,5];
+
+function sum (n) {
+        var summation = 0;
+
+        //do the actual sum
+        for (i=0; i<n.length;i++) {summation += n[i]}
+        return summation;
+}
+
+console.log (sum (numbers) );
